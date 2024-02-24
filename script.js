f8dca87e5d970d5efcd5ad01d2318ab2ea98daa3
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-02-24 12:26:15 +0300

    pause song funcitonality

--- script.js
+++ script.js
@@ -98,16 +98,20 @@ const playSong = (id) => {
     userData.currentSong = song;
     playButton.classList.add('playing');
     audio.play();
-
-
 };
 
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    playButton.classList.remove('playing');
+    audio.pause();
+
+};
 
 const renderSongs = (array) =>
   {
     const songsHTML = array.map((song).join("") => {
         return `<li id='song-${song.id}' class='playlist-song'>
-        <button class='playlist-song-info'>
+        <button class='playlist-song-info' onclick = 'playSong(${song.id})'>
             <span class='playlist-song-title'>${song.title}</span>
             <span class='playlist-song-artist'>${song.artist}</span>
             <span class='playlist-song-duration'>${song.duration}</span>
@@ -122,6 +126,20 @@ const renderSongs = (array) =>
 
 playlistSongs.innerHTML = songsHTML;
 };
+
+playButton.addEventListener('click',() => {
+    if (userData?.currentSong === null){
+    playSong(userData?.songs[0].id)
+    } else {
+    playSong(userData?.currentSong.id)
+    }
+};
+)
+
+
+
+pauseButton.addEventListener('click',pauseSong);
+
 
 userData?.songs.sort((a,b) => {
     if (a.title<b.title){
